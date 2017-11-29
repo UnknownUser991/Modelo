77d3192011ba8d2c2c34298cddb12cfc4edf5149
UnknownUser991 <33682940+UnknownUser991@users.noreply.github.com>
2017-11-29 09:05:17 +0100

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,14 @@
+x=int(input("Numero1"))
+y=int(input("Numero2"))
+z=int(input("Numero3"))
+
+if x < y < z:
+  print(x)
+  
+elif y < z < x:
+   print(y)
+   
+elif z < y < x:
+   print(z)
+   
+   
