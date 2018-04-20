37d4070ac41c2a64084b4441fac260b71288642e
UnknownUser991 <33682940+UnknownUser991@users.noreply.github.com>
2018-04-20 10:55:02 +0200

    Add files via upload

---
+++ P06_P4.py
@@ -0,0 +1,14 @@
+
+numero=1
+contador= -1  #-1 debido a que no se cuenta el numero 0
+while (numero != 0):
+  numero = int(input("Num?"))
+  contador = contador + 1 
+print(contador)
+  
+  
+  
+
+
+
+
